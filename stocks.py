5c9e0b4ae92b54dfd213389526a00689921f032d
adityanaidu16 <66760282+adityanaidu16@users.noreply.github.com>
2022-05-29 15:47:17 -0500

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -1,61 +1,225 @@
-import yfinance as yf
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-import plotly.graph_objects as go
-from sklearn.preprocessing import MinMaxScaler 
-import pickle 
-from tqdm.notebook import tnrange
-import requests 
+from qiskit_finance import QiskitFinanceError
+from qiskit_finance.data_providers import *
+import datetime
+# External imports
+from pylab import cm
+import pandas as pd
+from sklearn import metrics
+import numpy as np
+import matplotlib.pyplot as plt
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# Qiskit imports
+from qiskit import QuantumCircuit
+from qiskit.circuit import ParameterVector
+from qiskit.providers.aer import AerSimulator
+from qiskit.visualization import circuit_drawer
+from qiskit.algorithms.optimizers import SPSA
+from qiskit.circuit.library import ZZFeatureMap
+from qiskit_machine_learning.kernels import QuantumKernel
+from qiskit_machine_learning.kernels.algorithms import QuantumKernelTrainer
+from qiskit_machine_learning.algorithms import QSVR
+from qiskit_machine_learning.datasets import ad_hoc_data
+
+
+# In[5]:
+
+
+from qiskit_finance import QiskitFinanceError
+from qiskit_finance.data_providers import *
 
 def find_predicted_price(stock_symbol):
-    data = yf.download(stock_symbol , start = "2018-01-01" , interval = '1d')
+            #!/usr/bin/env python
+    # coding: utf-8
+
+    # In[1]:
+
+
+    get_ipython().system('pip install qiskit_finance')
+
+
+    # In[1]:
+
+
+    from qiskit import IBMQ
+
+
+    # In[2]:
+
+
+    IBMQ.save_account('72a2b314d20f97dd10cee423be6dbae1fe8a2b8a5b6181531c77501c2ca689cae045d13f9ea289e47968faede9fdd55a7a1e22e90725aada68dc2348f67ffe66')
+
+
+    # In[3]:
+
+
+    IBMQ.load_account()
+
+
+    # In[ ]:
+
+
+
+
+
+    # Loading main data
+
+    # In[4]:
+    dt = YahooDataProvider(tickers= "SPY",
+                     start = datetime.datetime(2016, 1, 1),
+                     end = datetime.datetime(2022, 5, 30))
+    dt.run()
+    data = pd.DataFrame(dt._data)
+    data = data.T
     data.head(3)
+
+
+    # In[6]:
+
+
+    # Sort the data points based on indexes just for confirmation 
     data.sort_index(inplace = True)
+
+
+    # In[7]:
+
+
+    # Remove any duplicate index 
     data = data.loc[~data.index.duplicated(keep='first')]
+
+
+    # In[8]:
+
+
     data.tail(3)
+
+
+    # In[9]:
+
+
     data.head()
+
+
+    # In[10]:
+
+
+    # Check for missing values 
     data.isnull().sum()
+
+
+    # In[11]:
+
+
+    # Get the statistics of the data
     data.describe()
-    fig = go.Figure()
-
-    fig.add_trace(go.Scatter(x = data.index , y = data['Close'] , mode = 'lines'))
-    fig.update_layout(height = 500 , width = 900, 
-                    xaxis_title='Date' , yaxis_title='Close')
-
-    fig = go.Figure()
-
-    fig.add_trace(go.Scatter(x = data.index , y = data['Volume'] , mode = 'lines'))
-    fig.update_layout(height = 500 , width = 900, 
-                    xaxis_title='Date' , yaxis_title='Volume')
 
 
-    data = data[['Close' , 'Volume']]
+    # Understanding Trends with in the Data
+
+    # In[12]:
+
+
+    get_ipython().system('pip install plotly')
+    import plotly.graph_objects as go
+
+
+    # In[13]:
+
+
+    # Check the trend in Closing Values 
+    data['Adj Close'].plot(figsize=(16,6))
+
+
+    # Data Preparation
+
+    # In[14]:
+
+
+    from sklearn.preprocessing import MinMaxScaler 
+    import pickle 
+    from tqdm.notebook import tnrange
+
+
+    # In[15]:
+
+
+    # Filter only required data 
+    data = data[['Adj Close']]
     data.head(3)
 
+
+    # Scrapping extra information
+
+    # In[16]:
+
+
+    import requests 
 
     response = requests.get('https://www.alphavantage.co/query?function=RSI&symbol=GOOGL&interval=daily&time_period=5&series_type=close&apikey=43T9T17VCV2ME4SM') 
     response = response.json()
 
 
+    # In[17]:
+
+
     response.keys()
+
+
+    # In[18]:
+
 
     rsi_data = pd.DataFrame.from_dict(response['Technical Analysis: RSI'] , orient='index')
 
+
+    # In[19]:
+
+
     rsi_data.head()
+
+
+    # In[20]:
+
 
     rsi_data = rsi_data[rsi_data.index >= '2018-01-01']
 
+
+    # In[21]:
+
+
     rsi_data['RSI'] = rsi_data['RSI'].astype(np.float64)
+
+
+    # In[22]:
+
 
     rsi_data.head()
 
+
+    # In[23]:
+
+
     data = data.merge(rsi_data, left_index=True, right_index=True, how='inner')
+
+
+    # In[24]:
+
 
     data.head()
 
+
+    # In[25]:
+
+
     # Confirm the Testing Set length 
     test_length = data[(data.index >= '2020-09-01')].shape[0]
+
+
+    # In[26]:
+
 
     def CreateFeatures_and_Targets(data, feature_length):
         X = []
@@ -63,7 +227,7 @@ def find_predicted_price(stock_symbol):
 
         for i in tnrange(len(data) - feature_length): 
             X.append(data.iloc[i : i + feature_length,:].values)
-            Y.append(data["Close"].values[i+feature_length])
+            Y.append(data["Adj Close"].values[i+feature_length])
 
         X = np.array(X)
         Y = np.array(Y)
@@ -71,182 +235,85 @@ def find_predicted_price(stock_symbol):
         return X , Y
 
 
+    # In[27]:
+
+
     X , Y = CreateFeatures_and_Targets(data , 32)
+
+
+    # In[ ]:
+
+
+
+
+
+    # In[28]:
+
 
     # Check the shapes
     X.shape , Y.shape
 
 
+    # In[29]:
+
+
     Xtrain , Xtest , Ytrain , Ytest = X[:-test_length] , X[-test_length:] , Y[:-test_length] , Y[-test_length:]
+
+
+    # In[30]:
+
 
     # Check Training Dataset Shape 
     Xtrain.shape , Ytrain.shape
+
+
+    # In[31]:
+
 
     # Check Testing Dataset Shape
     Xtest.shape , Ytest.shape
 
 
-    # Create a Scaler to Scale Vectors with Multiple Dimensions 
-    class MultiDimensionScaler():
-        def __init__(self):
-            self.scalers = []
+    # In[32]:
 
-        def fit_transform(self , X):
-            total_dims = X.shape[2]
-            for i in range(total_dims):
-                Scaler = MinMaxScaler()
-                X[:, :, i] = Scaler.fit_transform(X[:,:,i])
-                self.scalers.append(Scaler)
-            return X
 
-        def transform(self , X):
-            for i in range(X.shape[2]):
-                X[:, :, i] = self.scalers[i].transform(X[:,:,i])
-            return X 
+    from qiskit_machine_learning.algorithms import VQC
+    # import the feature map and ansatz circuits
+    from qiskit.circuit.library import ZZFeatureMap, RealAmplitudes
+    # import the optimizer for the training
+    from qiskit.algorithms.optimizers import L_BFGS_B
+    # import backend
+    from qiskit.providers.aer import QasmSimulator
 
-    Feature_Scaler = MultiDimensionScaler()
-    Xtrain = Feature_Scaler.fit_transform(Xtrain)
-    Xtest = Feature_Scaler.transform(Xtest)
 
-    Target_Scaler = MinMaxScaler()
-    Ytrain = Target_Scaler.fit_transform(Ytrain.reshape(-1,1))
-    Ytest = Target_Scaler.transform(Ytest.reshape(-1,1))
+    # In[33]:
 
 
-    def save_object(obj , name : str):
-        pickle_out = open(f"{name}.pck","wb")
-        pickle.dump(obj, pickle_out)
-        pickle_out.close()
+    num_qubits = 2
+    vqc = VQC(feature_map=ZZFeatureMap(num_qubits), 
+              ansatz=RealAmplitudes(num_qubits, reps=1), 
+              loss='cross_entropy', 
+              optimizer=L_BFGS_B(),
+              quantum_instance=QasmSimulator())
+    # train classifier
+    vqc.fit(Xtrain, Ytrain)
+    # score result
+    vqc.score(Xtest, Ytest)
 
-    def load_object(name : str):
-        pickle_in = open(f"{name}.pck","rb")
-        data = pickle.load(pickle_in)
-        return data
 
+    # In[ ]:
 
-    # Save your objects for future purposes 
-    save_object(Feature_Scaler , "Feature_Scaler")
-    save_object(Target_Scaler , "Target_Scaler")
 
 
-    # Model Building
 
 
-    from tensorflow.keras.callbacks import ModelCheckpoint , ReduceLROnPlateau
+    # In[ ]:
 
-    save_best = ModelCheckpoint("best_weights.h5", monitor='val_loss', save_best_only=True, save_weights_only=True)
-    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.25,patience=5, min_lr=0.00001,verbose = 1)
 
+    VQC['Adj Close'].plot(figsize=(16,6)
 
-    from tensorflow.keras.models import Sequential
-    from tensorflow.keras.layers import Dense , Dropout , LSTM , Bidirectional , BatchNormalization
 
-    model = Sequential()
-
-    model.add(Bidirectional(LSTM(512 ,return_sequences=True , recurrent_dropout=0.1, input_shape=(32, 3))))
-    model.add(LSTM(256 ,recurrent_dropout=0.1))
-    model.add(Dropout(0.3))
-    model.add(Dense(64 , activation='elu'))
-    model.add(Dropout(0.3))
-    model.add(Dense(32 , activation='elu'))
-    model.add(Dense(1 , activation='linear'))
-
-
-
-    optimizer = tf.keras.optimizers.SGD(learning_rate = 0.002)
-    model.compile(loss='mse', optimizer=optimizer)
-
-
-
-    history = model.fit(Xtrain, Ytrain,
-                epochs=1,
-                batch_size = 1,
-                verbose=1,
-                shuffle=False ,
-                validation_data=(Xtest , Ytest),
-                callbacks=[reduce_lr , save_best])
-
-
-
-    # Load the best weights
-    model.load_weights("best_weights.h5")
-
-
-    # Visualize prediction on Test Set
-
-
-    Predictions = model.predict(Xtest)
-
-
-    Predictions = Target_Scaler.inverse_transform(Predictions)
-    Actual = Target_Scaler.inverse_transform(Ytest)
-
-
-    Predictions.shape
-
-
-    Predictions = np.squeeze(Predictions , axis = 1)
-    Actual = np.squeeze(Actual , axis = 1)
-
-
-
-    # Check the Predictions vs Actual
-    fig = go.Figure()
-
-    fig.add_trace(go.Scatter(x = data.index[-test_length:] , y = Actual , mode = 'lines' , name='Actual'))
-    fig.add_trace(go.Scatter(x = data.index[-test_length:] , y = Predictions , mode = 'lines' , name='Predicted'))
-    #fig.show()
-
-
-    # Visualize Prediction on whole data
-
-
-    Total_features = np.concatenate((Xtrain , Xtest) , axis = 0)
-
-
-
-    Total_Targets = np.concatenate((Ytrain , Ytest) , axis = 0)
-
-
-
-    Predictions = model.predict(Total_features)
-
-
-    Predictions = Target_Scaler.inverse_transform(Predictions)
-    Actual = Target_Scaler.inverse_transform(Total_Targets)
-
-
-    Predictions = np.squeeze(Predictions , axis = 1)
-    Actual = np.squeeze(Actual , axis = 1)
-
-
-
-    # Check the trend in Volume Traded
-    fig = go.Figure()
-
-    fig.add_trace(go.Scatter(x = data.index , y = Actual , mode = 'lines' , name='Actual'))
-    fig.add_trace(go.Scatter(x = data.index , y = Predictions , mode = 'lines' , name='Predicted'))
-
-
-    # Save and Load the whole model
-    model.save("Model.h5")
-    loaded_model = tf.keras.models.load_model("Model.h5")
-
-
-    # Realtime Prediction
-
-    def PredictStockPrice(Model , DataFrame , PreviousDate , feature_length = 32):
-        idx_location = DataFrame.index.get_loc(PreviousDate)
-        Features = DataFrame.iloc[idx_location - feature_length : idx_location,:].values
-        Features = np.expand_dims(Features , axis = 0)
-        Features = Feature_Scaler.transform(Features)
-        Prediction = Model.predict(Features)
-        Prediction = Target_Scaler.inverse_transform(Prediction)
-        return Prediction[0][0]
-
-
-
-    predicted_price= PredictStockPrice(loaded_model , data , '2022-05-27')
+    # In[ ]:
     return predicted_price
 
 
